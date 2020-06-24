72d471c5d4a3c95dee6ed41efeae2b52440a0704
Nafeu Nasir <nafeu.nasir@gmail.com>
2020-06-24 19:35:08 -0400

    Add placeholder send-mail api endpoint

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ const bodyParser = require('body-parser');
 const app = express();
 const server = http.Server(app);
 
-server.listen(process.env.PORT || 8000, function(){
-  console.log('[ server.js ] Listening on port ' + server.address().port);
+server.listen(process.env.PORT || 8000, () => {
+  console.log(`[ server.js ] Listening on port ${server.address().port}`);
 });
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/api/', function(req, res){
+app.get('/api/', (req, res) => {
+  res.status(200).send('OK');
+});
+
+app.post('/api/send-email', (req, res) => {
   res.status(200).send('OK');
 });
