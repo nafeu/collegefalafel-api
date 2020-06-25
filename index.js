285da633574c3a32de005604815fd065b04dc6a5
Nafeu Nasir <nafeu.nasir@gmail.com>
2020-06-24 20:09:51 -0400

    Integrate sendgrid

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const bodyParser = require('body-parser');
 const app = express();
 const server = http.Server(app);
 
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 server.listen(process.env.PORT || 8000, () => {
   console.log(`[ server.js ] Listening on port ${server.address().port}`);
 });
@@ -16,6 +20,20 @@ app.get('/api/', (req, res) => {
   res.status(200).send('OK');
 });
 
-app.post('/api/send-email', (req, res) => {
+app.post('/api/send-email', async (req, res) => {
+  const msg = {
+    to: 'nafeu.nasir@gmail.com',
+    from: 'mythic47@hotmail.com',
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+
+  try {
+    await sgMail.send(msg);
+  } catch (error) {
+    console.log(JSON.stringify({ error }, null, 2));
+  }
+
   res.status(200).send('OK');
 });
